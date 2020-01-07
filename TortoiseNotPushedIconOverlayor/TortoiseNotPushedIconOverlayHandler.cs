6c194568ed623df6d8261b3eb77ca55f8ab2754d
蒋龙 <372792797@qq.com>
2020-01-07 16:35:22 +0800

    files sort

---
+++ TortoiseNotPushedIconOverlayor/TortoiseNotPushedIconOverlayHandler.cs
@@ -0,0 +1,35 @@
+﻿using CommonUtils;
+using SharpShell.Attributes;
+using SharpShell.Interop;
+using SharpShell.SharpIconOverlayHandler;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace TortoiseNotPushedIconOverlayor
+{
+    [ComVisible(true)]
+    [RegistrationName("  Tortoise0NotPushed")]
+    class TortoiseNotPushedIconOverlayHandler : SharpIconOverlayHandler
+    {
+        protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes)
+        {
+            LogUtil.Log(path);
+            return true;
+        }
+
+        private static Icon NotPushIcon = new Icon(@"D:\Users\jianglong\Pictures\XPStyle\NotPush.ico");
+
+        protected override Icon GetOverlayIcon()
+        {
+            return NotPushIcon;
+        }
+
+        /// <summary>
+        /// 优先级 0-100 0最高
+        /// </summary>
+        protected override int GetPriority()
+        {
+            return 0;
+        }
+    }
+}
