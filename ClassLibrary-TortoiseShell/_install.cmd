d1ad3ccec5fc0b7cffeea2f52cac3e6b5d247cf9
蒋龙 <372792797@qq.com>
2020-01-07 15:14:26 +0800

    cmd

--- ClassLibrary-TortoiseShell/_install.cmd
+++ ClassLibrary-TortoiseShell/_install.cmd
@@ -1,4 +1,4 @@
 echo 请在管理员模式下运行
 pause
-%windir%\regasm.exe ClassLibrary-TortoiseShell.dll /codebase
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe D:\Subversion\TortoiseShell\ClassLibrary-TortoiseShell\bin\Debug\ClassLibrary-TortoiseShell.dll /codebase
 pause
