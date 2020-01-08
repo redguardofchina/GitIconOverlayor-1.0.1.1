0e9c181283f81e5341dcadd6b498122d8798ea4c
蒋龙 <372792797@qq.com>
2020-01-08 13:14:00 +0800

    TortoiseGitNotPushedIconOverlayHandler

--- TortoiseGitNotPushedIconOverlayHandler/TortoiseGitNotPushedIconOverlayHandler.cs
+++ TortoiseGitNotPushedIconOverlayHandler/TortoiseGitNotPushedIconOverlayHandler.cs
@@ -13,6 +13,8 @@ namespace TortoiseGitNotPushedIconOverlayHandler
     {
         private static bool _hasGit = !CommandUtil.Run("git").HasError;
 
+        private const string _keywords = "(use \"git push\" to publish your local commits)";
+
         protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes)
         {
             if (!_hasGit)
@@ -21,10 +23,14 @@ namespace TortoiseGitNotPushedIconOverlayHandler
             if (!FileSystem.IsFloder(path))
                 return false;
 
-            if (FloderUtil.Exists(path.Combine(".git")))
-                return true;
+            if (!FloderUtil.Exists(path.Combine(".git")))
+                return false;
 
-            return false;
+            var result = CommandUtil.Run("git status", path);
+            if (result.HasError)
+                return false;
+
+            return result.Result.Contains(_keywords);
         }
 
         private static Icon _notPushedIcon = new Icon(ResourceUtil.ReadStream("NotPushed.ico"));
