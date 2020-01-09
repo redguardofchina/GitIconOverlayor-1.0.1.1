e6487be546a268c55fc247dd8bdb3c41f703d162
蒋龙 <372792797@qq.com>
2020-01-09 17:09:24 +0800

    add test

---
+++ ConsoleApp-Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using CommonUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TortoiseGitNotPushedIconOverlayHandler;
+
+namespace ConsoleApp_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                LogUtil.Print(GitUtil.GetStatus(@"D:\Subversion\CommonUtils-dot-net"));
+            }
+        }
+    }
+}
