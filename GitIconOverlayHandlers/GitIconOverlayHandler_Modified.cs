1bbfdce074717c8af6db28e91c3fdf484a2c9612
蒋龙 <372792797@qq.com>
2020-01-09 20:58:07 +0800

    add GitIconOverlayHandler_Modified

---
+++ GitIconOverlayHandlers/GitIconOverlayHandler_Modified.cs
@@ -0,0 +1,31 @@
+﻿using SharpShell.Attributes;
+using SharpShell.Interop;
+using SharpShell.SharpIconOverlayHandler;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace GitIconOverlayHandlers
+{
+    [ComVisible(true)]
+    [RegistrationName("  GitModified")]
+    public class GitIconOverlayHandler_Modified : SharpIconOverlayHandler
+    {
+        protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes)
+        {
+            //LogUtil.Log("CanShowOverlay");
+            return IconOverlay.CanShow(path, GitStatus.Modified);
+        }
+
+        protected override Icon GetOverlayIcon()
+        {
+            //LogUtil.Log("GetOverlayIcon");
+            return Resources.ModifiedIcon;
+        }
+
+        protected override int GetPriority()
+        {
+            //LogUtil.Log("GetPriority");
+            return IconOverlay.Priority;
+        }
+    }
+}
