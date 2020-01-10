78716c26604449fa79e78630dbe80a4086f0ae3d
蒋龙 <372792797@qq.com>
2020-01-10 18:09:41 +0800

    add GitIconOverlayHandler_Conflict

---
+++ GitIconOverlayHandlers/GitIconOverlayHandler_Conflict.cs
@@ -0,0 +1,32 @@
+﻿using CommonUtils;
+using SharpShell.Attributes;
+using SharpShell.Interop;
+using SharpShell.SharpIconOverlayHandler;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace GitIconOverlayHandlers
+{
+    [ComVisible(true)]
+    [RegistrationName("  GitConflict")]
+    public class GitIconOverlayHandler_Conflict : SharpIconOverlayHandler
+    {
+        protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes)
+        {
+            //LogUtil.Log("CanShowOverlay");
+            return GitUtil.GetStatus(path) == GitStatus.Conflict;
+        }
+
+        protected override Icon GetOverlayIcon()
+        {
+            //LogUtil.Log("GetOverlayIcon");
+            return Resources.ConflictIcon;
+        }
+
+        protected override int GetPriority()
+        {
+            //LogUtil.Log("GetPriority");
+            return IconOverlay.Priority;
+        }
+    }
+}
--- GitIconOverlayHandlers/Resources.cs
+++ GitIconOverlayHandlers/Resources.cs
@@ -6,7 +6,13 @@ namespace GitIconOverlayHandlers
     public static class Resources
     {
         public static readonly Icon ModifiedIcon = new Icon(ResourceUtil.ReadStream("resources.ModifiedIcon.ico"));
+        public static readonly Icon ConflictIcon = new Icon(ResourceUtil.ReadStream("resources.ConflictIcon.ico"));
         public static readonly Icon CommittedIcon = new Icon(ResourceUtil.ReadStream("resources.CommittedIcon.ico"));
         public static readonly Icon PushedIcon = new Icon(ResourceUtil.ReadStream("resources.PushedIcon.ico"));
+
+        public static readonly Icon ModifiedIconWithUpdate = new Icon(ResourceUtil.ReadStream("resources.ModifiedIcon-u.ico"));
+        public static readonly Icon ConflictIconWithUpdate = new Icon(ResourceUtil.ReadStream("resources.ConflictIcon-u.ico"));
+        public static readonly Icon CommittedIconWithUpdate = new Icon(ResourceUtil.ReadStream("resources.CommittedIcon-u.ico"));
+        public static readonly Icon PushedIconWithUpdate = new Icon(ResourceUtil.ReadStream("resources.PushedIcon-u.ico"));
     }
 }
